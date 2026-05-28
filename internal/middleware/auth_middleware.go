0057090e980b0075bfa539b9c9a292f15ef3a247
wahcah develop <wahcahdevelop@gmail.com>
2026-05-28 12:16:28 +0000

    optional auth

--- internal/middleware/auth_middleware.go
+++ internal/middleware/auth_middleware.go
@@ -32,3 +32,30 @@ func Protected() fiber.Handler {
 		return c.Next()
 	}
 }
+
+func OptionalAuth() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		authHeader := c.Get("Authorization")
+
+		// Tidak ada token? Lanjut saja tanpa set user_id
+		if authHeader == "" {
+			return c.Next()
+		}
+
+		tokenString := strings.Replace(authHeader, "Bearer ", "", 1)
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("JWT_SECRET")), nil
+		})
+
+		// Token ada tapi invalid? Tetap lanjut (atau bisa juga di-reject, tergantung kebutuhan)
+		if err != nil || !token.Valid {
+			return c.Next()
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+		c.Locals("user_id", uint(claims["user_id"].(float64)))
+
+		return c.Next()
+	}
+}
